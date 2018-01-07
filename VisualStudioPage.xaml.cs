e88147a913d2189d034419ff698cecf9fe0cbd12
jkmco <jackcakal@gmail.com>
2018-01-07 15:39:35 +0800

    Initial check-in of module VisualStudio

---
+++ VisualStudioPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace VisualStudio
+{
+    public partial class VisualStudioPage : ContentPage
+    {
+        public VisualStudioPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
